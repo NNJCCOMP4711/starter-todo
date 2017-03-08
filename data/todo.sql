1e3a0ee28af89c55eddd1b25145dbf8057b4279d
ntorin <xtoribear@gmail.com>
2017-03-08 10:03:49 -0800

    Job 8 - Use an RDB for Sessions

--- data/todo.sql
+++ data/todo.sql
@@ -1,218 +1,208 @@
--- phpMyAdmin SQL Dump
--- version 4.6.3
--- https://www.phpmyadmin.net/
+CREATE DATABASE  IF NOT EXISTS `todo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `todo`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
--- Host: localhost
--- Generation Time: Feb 14, 2017 at 08:40 AM
--- Server version: 5.7.13
--- PHP Version: 7.0.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: todo
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `todo`
+-- Table structure for table `ci_sessions`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `ci_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+LOCK TABLES `ci_sessions` WRITE;
+/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+INSERT INTO `ci_sessions` VALUES ('0b73tij5tqg92qu3t2774gvrtt3o1lmv','127.0.0.1',1488995890,'__ci_last_regenerate|i:1488995890;');
+/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `flags`
 --
 
 DROP TABLE IF EXISTS `flags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `flags` (
   `id` int(1) NOT NULL,
-  `meaning` varchar(6) DEFAULT NULL
+  `meaning` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `id_2` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `flags`
 --
 
-INSERT INTO `flags` (`id`, `meaning`) VALUES
-(1, 'Urgent');
-
--- --------------------------------------------------------
+LOCK TABLES `flags` WRITE;
+/*!40000 ALTER TABLE `flags` DISABLE KEYS */;
+INSERT INTO `flags` VALUES (1,'Urgent');
+/*!40000 ALTER TABLE `flags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `groups`
 --
 
 DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `groups` (
   `id` int(1) NOT NULL,
-  `name` varchar(6) DEFAULT NULL
+  `name` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `groups`
 --
 
-INSERT INTO `groups` (`id`, `name`) VALUES
-(1, 'house'),
-(2, 'school'),
-(3, 'work'),
-(4, 'family');
-
--- --------------------------------------------------------
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'house'),(2,'school'),(3,'work'),(4,'family');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `priorities`
 --
 
 DROP TABLE IF EXISTS `priorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `priorities` (
   `id` int(1) NOT NULL,
-  `name` varchar(6) DEFAULT NULL
+  `name` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `priorities`
 --
 
-INSERT INTO `priorities` (`id`, `name`) VALUES
-(1, 'low'),
-(2, 'medium'),
-(3, 'high');
-
--- --------------------------------------------------------
+LOCK TABLES `priorities` WRITE;
+/*!40000 ALTER TABLE `priorities` DISABLE KEYS */;
+INSERT INTO `priorities` VALUES (1,'low'),(2,'medium'),(3,'high');
+/*!40000 ALTER TABLE `priorities` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `sizes`
 --
 
 DROP TABLE IF EXISTS `sizes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sizes` (
   `id` int(1) NOT NULL,
-  `name` varchar(6) DEFAULT NULL
+  `name` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `sizes`
 --
 
-INSERT INTO `sizes` (`id`, `name`) VALUES
-(1, 'small'),
-(2, 'medium'),
-(3, 'large');
-
--- --------------------------------------------------------
+LOCK TABLES `sizes` WRITE;
+/*!40000 ALTER TABLE `sizes` DISABLE KEYS */;
+INSERT INTO `sizes` VALUES (1,'small'),(2,'medium'),(3,'large');
+/*!40000 ALTER TABLE `sizes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `statuses`
 --
 
 DROP TABLE IF EXISTS `statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statuses` (
   `id` int(1) NOT NULL,
-  `name` varchar(11) DEFAULT NULL
+  `name` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `statuses`
 --
 
-INSERT INTO `statuses` (`id`, `name`) VALUES
-(1, 'in progress'),
-(2, 'complete');
-
--- --------------------------------------------------------
+LOCK TABLES `statuses` WRITE;
+/*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
+INSERT INTO `statuses` VALUES (1,'in progress'),(2,'complete');
+/*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tasks`
 --
 
 DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tasks` (
-  `id` int(2) NOT NULL,
+  `id` int(2) NOT NULL AUTO_INCREMENT,
   `task` varchar(22) DEFAULT NULL,
   `priority` int(1) DEFAULT NULL,
   `size` int(1) DEFAULT NULL,
   `group` int(1) DEFAULT NULL,
   `deadline` varchar(8) DEFAULT NULL,
   `status` varchar(1) DEFAULT NULL,
-  `flag` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `flag` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `task`, `priority`, `size`, `group`, `deadline`, `status`, `flag`) VALUES
-(1, 'COMP1234 assignment', 3, 2, 2, '20170219', '1', ''),
-(2, 'Mow the lawn', 2, 2, 1, '', '2', ''),
-(3, 'Wash the car', 2, 2, 1, '', '', ''),
-(4, 'Paint the fence', 1, 2, 1, '', '', ''),
-(5, 'Study for midterms', 3, 3, 2, '', '', ''),
-(6, 'Intramural hockey game', 1, 2, 4, '', '', ''),
-(7, 'Canucks hockey game', 3, 3, 4, '20170305', '', ''),
-(8, 'Buy steel-toed boots', 2, 1, 3, '', '', ''),
-(9, 'Learn French', 1, 3, 3, '20161231', '1', ''),
-(10, 'Hit the gym', 2, 1, 4, '', '', ''),
-(11, 'Pay bills', 3, 1, 1, '', '', ''),
-(12, 'Meet George', 2, 1, 1, '', '', ''),
-(13, 'Buy milk & bread', 2, 1, 1, '', '', '1'),
-(14, 'Read War & Peace', 1, 3, 1, '', '', ''),
-(15, 'Organize the study', 1, 4, 1, '', '', '');
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'COMP1234 assignment',3,2,2,'20170219','1',''),(2,'Mow the lawn',2,2,1,'','2',''),(3,'Wash the car',2,2,1,'','',''),(4,'Paint the fence',1,2,1,'','',''),(5,'Study for midterms',3,3,2,'','',''),(6,'Intramural hockey game',1,2,4,'','',''),(7,'Canucks hockey game',3,3,4,'20170305','',''),(8,'Buy steel-toed boots',2,1,3,'','',''),(9,'Learn French',1,3,3,'20161231','1',''),(10,'Hit the gym',2,1,4,'','',''),(11,'Pay bills',3,1,1,'','',''),(12,'Meet George',2,1,1,'','',''),(13,'Buy milk & bread',2,1,1,'','','1'),(14,'Read War & Peace',1,3,1,'','',''),(15,'Organize the study',1,4,1,'','','');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `flags`
---
-ALTER TABLE `flags`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `id_2` (`id`);
-
---
--- Indexes for table `groups`
---
-ALTER TABLE `groups`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `priorities`
---
-ALTER TABLE `priorities`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `sizes`
---
-ALTER TABLE `sizes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `statuses`
---
-ALTER TABLE `statuses`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-08 10:01:36
